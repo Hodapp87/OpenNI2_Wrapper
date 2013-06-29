8f954abe0a67556f4197326a6695478e35b493b7
Chris Hodapp <Hodapp87@gmail.com>
2013-06-29 12:15:27 -0400

    Progressed with OpenNI2 C wrappers

--- openni2_c_wrapper.cpp
+++ openni2_c_wrapper.cpp
@@ -15,21 +15,28 @@
         std::cout << "Error: " << e.what() << std::endl; \
     }
 
-int oni_IMAGE_REGISTRATION_OFF = openni::IMAGE_REGISTRATION_OFF;
-int oni_IMAGE_REGISTRATION_DEPTH_TO_COLOR = openni::IMAGE_REGISTRATION_DEPTH_TO_COLOR;
+// ==========================
+// Static constants for enums
+// ==========================
+oni_ImageRegistrationMode oni_IMAGE_REGISTRATION_OFF = openni::IMAGE_REGISTRATION_OFF;
+oni_ImageRegistrationMode oni_IMAGE_REGISTRATION_DEPTH_TO_COLOR = openni::IMAGE_REGISTRATION_DEPTH_TO_COLOR;
 
-int oni_STATUS_OK = openni::STATUS_OK;
-int oni_STATUS_ERROR = openni::STATUS_ERROR;
-int oni_STATUS_NOT_IMPLEMENTED = openni::STATUS_NOT_IMPLEMENTED;
-int oni_STATUS_NOT_SUPPORTED = openni::STATUS_NOT_SUPPORTED;
-int oni_STATUS_BAD_PARAMETER = openni::STATUS_BAD_PARAMETER;
-int oni_STATUS_OUT_OF_FLOW = openni::STATUS_OUT_OF_FLOW;
-int oni_STATUS_NO_DEVICE = openni::STATUS_NO_DEVICE;
-int oni_STATUS_TIME_OUT = openni::STATUS_TIME_OUT;
+oni_Status oni_STATUS_OK = openni::STATUS_OK;
+oni_Status oni_STATUS_ERROR = openni::STATUS_ERROR;
+oni_Status oni_STATUS_NOT_IMPLEMENTED = openni::STATUS_NOT_IMPLEMENTED;
+oni_Status oni_STATUS_NOT_SUPPORTED = openni::STATUS_NOT_SUPPORTED;
+oni_Status oni_STATUS_BAD_PARAMETER = openni::STATUS_BAD_PARAMETER;
+oni_Status oni_STATUS_OUT_OF_FLOW = openni::STATUS_OUT_OF_FLOW;
+oni_Status oni_STATUS_NO_DEVICE = openni::STATUS_NO_DEVICE;
+oni_Status oni_STATUS_TIME_OUT = openni::STATUS_TIME_OUT;
 
-// =============
-// openni::Camera
-// =============
+oni_SensorType oni_SENSOR_IR = openni::SENSOR_IR;
+oni_SensorType oni_SENSOR_COLOR = openni::SENSOR_COLOR;
+oni_SensorType oni_SENSOR_DEPTH = openni::SENSOR_DEPTH;
+
+// ======================
+// openni::CameraSettings
+// ======================
 bool oni_getAutoExposureEnabled(oni_CameraSettings * camera) {
     EXC_CHECK( return camera->getAutoExposureEnabled(); );
     return false;
@@ -40,7 +47,7 @@ bool oni_getAutoWhiteBalanceEnabled(oni_CameraSettings * camera) {
     return false;
 }
 
-bool oni_isValid(oni_CameraSettings * camera) {
+bool oni_CameraSettings_isValid(oni_CameraSettings * camera) {
     EXC_CHECK( return camera->isValid(); );
     return false;
 }
@@ -55,17 +62,54 @@ oni_Status oni_setAutoWhiteBalanceEnabled(oni_CameraSettings * camera, bool enab
     return openni::STATUS_ERROR;
 }
 
-// ==============
-// openni::OpenNI
-// ==============
-oni_Status oni_initialize() {
-    EXC_CHECK( return openni::OpenNI::initialize(); );
+// ===========================
+// openni::CoordinateConverter
+// ===========================
+oni_Status oni_convertDepthToColor(oni_VideoStream * depth,
+                                   oni_VideoStream * color, int depthX,
+                                   int depthY, oni_DepthPixel depthZ,
+                                   int *pColorX, int *pColorY)
+{
+    EXC_CHECK(return openni::CoordinateConverter::convertDepthToColor(
+                  *depth, *color, depthX, depthY, depthZ, pColorX, pColorY);)
     return openni::STATUS_ERROR;
 }
 
-const char * oni_getExtendedError() {
-    EXC_CHECK( return openni::OpenNI::getExtendedError(); );
-    return NULL;
+oni_Status oni_convertDepthToWorld1(oni_VideoStream * depth, int depthX,
+                                    int depthY, oni_DepthPixel depthZ,
+                                    float *pWorldX, float *pWorldY,
+                                    float *pWorldZ)
+{
+    EXC_CHECK(return openni::CoordinateConverter::convertDepthToWorld(
+                  *depth, depthX, depthY, depthZ, pWorldX, pWorldY, pWorldZ);)
+    return openni::STATUS_ERROR;
+}
+
+oni_Status oni_convertDepthToWorld2(oni_VideoStream * depth, float depthX,
+                                    float depthY, float depthZ, float *pWorldX,
+                                    float *pWorldY, float *pWorldZ)
+{
+    EXC_CHECK(return openni::CoordinateConverter::convertDepthToWorld(
+                  *depth, depthX, depthY, depthZ, pWorldX, pWorldY, pWorldZ);)
+    return openni::STATUS_ERROR;
+}
+
+oni_Status oni_convertWorldToDepth1(oni_VideoStream * depth, float worldX,
+                                    float worldY, float worldZ, int *pDepthX,
+                                    int *pDepthY, oni_DepthPixel *pDepthZ)
+{
+    EXC_CHECK(return openni::CoordinateConverter::convertWorldToDepth(
+                  *depth, worldX, worldY, worldZ, pDepthX, pDepthY, pDepthZ);)
+    return openni::STATUS_ERROR;
+}
+
+oni_Status oni_convertWorldToDepth2(oni_VideoStream * depth, float worldX,
+                                    float worldY, float worldZ, float *pDepthX,
+                                    float *pDepthY, float *pDepthZ)
+{
+    EXC_CHECK(return openni::CoordinateConverter::convertWorldToDepth(
+                  *depth, worldX, worldY, worldZ, pDepthX, pDepthY, pDepthZ);)
+    return openni::STATUS_ERROR;
 }
 
 // ==============
@@ -93,8 +137,78 @@ oni_DeviceInfo * oni_getDeviceInfo(oni_Device * device) {
     return NULL;
 }
 
-int oni_open(oni_Device * device, const char * uri) {
+oni_ImageRegistrationMode oni_getImageRegistrationMode(oni_Device * device) {
+    EXC_CHECK( return device->getImageRegistrationMode(); )
+    return oni_IMAGE_REGISTRATION_OFF;
+}
+
+oni_PlaybackControl * oni_getPlaybackControl(oni_Device * device) {
+    EXC_CHECK( return device->getPlaybackControl(); )
+    return NULL;
+}
+
+oni_Status oni_getProperty(oni_Device * device, int propertyId, void * data, int * dataSize) {
+    EXC_CHECK( return device->getProperty(propertyId, data, dataSize); )
+    return openni::STATUS_ERROR;
+}
+
+const oni_SensorInfo * oni_getSensorInfo(oni_Device * device, oni_SensorType sensorType) {
+    EXC_CHECK( return device->getSensorInfo(sensorType); )
+    return NULL;
+}
+
+bool oni_hasSensor(oni_Device * device, oni_SensorType sensorType) {
+    EXC_CHECK( return device->hasSensor(sensorType); )
+    return false;
+}
+
+oni_Status oni_invoke(oni_Device * device, int cmdId, const void * data, int dataSize) {
+    EXC_CHECK( return device->invoke(cmdId, data, dataSize); )
+    return openni::STATUS_ERROR;
+}
+
+bool oni_isCommandSupported(oni_Device * device, int cmdId) {
+    EXC_CHECK( return device->isCommandSupported(cmdId); );
+    return false;
+}
+
+bool oni_isFile(oni_Device * device) {
+    EXC_CHECK( return device->isFile(); );
+    return false;
+}
+
+bool oni_isImageRegistrationModeSupported(oni_Device * device, oni_ImageRegistrationMode mode) {
+    EXC_CHECK( return device->isImageRegistrationModeSupported(mode); );
+    return false;
+}
+
+bool oni_isPropertySupported(oni_Device * device, int propertyId) {
+    EXC_CHECK( return device->isPropertySupported(propertyId); );
+    return false;
+}
+
+bool oni_Device_isValid(oni_Device * device) {
+    EXC_CHECK( return device->isValid(); );
+    return false;
+}
+
+oni_Status oni_open(oni_Device * device, const char * uri) {
     EXC_CHECK( return device->open(uri); );
+    return openni::STATUS_ERROR;
+}
+
+oni_Status oni_setDepthColorSyncEnabled(oni_Device * device, bool isEnabled) {
+    EXC_CHECK( return device->setDepthColorSyncEnabled(isEnabled); );
+    return openni::STATUS_ERROR;
+}
+
+oni_Status oni_setImageRegistrationMode(oni_Device * device, oni_ImageRegistrationMode mode) {
+    EXC_CHECK( return device->setImageRegistrationMode(mode); );
+    return openni::STATUS_ERROR;
+}
+
+oni_Status oni_setProperty(oni_Device * device, int propId, const void * data, int dataSize) {
+    EXC_CHECK( return device->setProperty(propId, data, dataSize); );
     return openni::STATUS_ERROR;
 }
 
@@ -126,3 +240,32 @@ const char * oni_getVendor(oni_DeviceInfo * info) {
     return NULL;
 }
 
+// ==============
+// openni::OpenNI
+// ==============
+/*oni_Status oni_addListener(oni_Listener * pListener) {
+    EXC_CHECK( return openni::OpenNI::addListener(pListener); );
+    return openni::STATUS_ERROR;
+}
+*/
+
+oni_Status oni_initialize() {
+    EXC_CHECK( return openni::OpenNI::initialize(); );
+    return openni::STATUS_ERROR;
+}
+
+const char * oni_getExtendedError() {
+    EXC_CHECK( return openni::OpenNI::getExtendedError(); );
+    return NULL;
+}
+
+oni_Version oni_getVersion() {
+    oni_Version ver_c;
+    openni::Version ver;
+    EXC_CHECK( ver = openni::OpenNI::getVersion(); );
+    ver_c.build = ver.build;
+    ver_c.maintenance = ver.maintenance;
+    ver_c.major = ver.major;
+    ver_c.minor = ver.minor;
+    return ver_c;
+}
--- openni2_c_wrapper.h
+++ openni2_c_wrapper.h
@@ -11,7 +11,7 @@ the naming in OpenNI2, except for a few things:
  - Using a suffix of "_new" and "_delete" for constructor and destructor,
 respectively.
  - Making some allowances for overloaded or templatized functions (still to be
-determined...)
+determined...), and for name conflicts (e.g. isValid)
 
 enums are wrapped as extern ints.
 
@@ -25,7 +25,12 @@ typedef openni::CameraSettings oni_CameraSettings;
 typedef openni::Device oni_Device;
 typedef openni::DeviceInfo oni_DeviceInfo;
 typedef openni::ImageRegistrationMode oni_ImageRegistrationMode;
+//typedef openni::OpenNI::Listener oni_Listener;
+// TODO Fix this (I don't know what the real Listener class is):
+typedef struct oni_Listener oni_Listener;
 typedef openni::PlaybackControl oni_PlaybackControl;
+typedef openni::SensorInfo oni_SensorInfo;
+typedef openni::SensorType oni_SensorType;
 typedef openni::Status oni_Status;
 typedef openni::VideoStream oni_VideoStream;
 typedef openni::DepthPixel oni_DepthPixel;
@@ -37,7 +42,9 @@ typedef struct Array Array;
 typedef struct oni_CameraSettings oni_CameraSettings;
 typedef struct oni_Device oni_Device;
 typedef struct oni_DeviceInfo oni_DeviceInfo;
+typedef struct oni_Listener oni_Listener;
 typedef struct oni_PlaybackControl oni_PlaybackControl;
+typedef struct oni_SensorInfo oni_SensorInfo;
 typedef struct oni_VideoStream oni_VideoStream;
 typedef unsigned short uint16_t;
 typedef uint16_t oni_DepthPixel;
@@ -48,6 +55,14 @@ typedef uint16_t oni_DepthPixel;
 typedef int oni_ImageRegistrationMode;
 extern const int oni_IMAGE_REGISTRATION_OFF;
 extern const int oni_IMAGE_REGISTRATION_DEPTH_TO_COLOR;
+
+// ==================
+// openni::SensorType
+// ==================
+typedef int oni_SensorType;
+extern const int oni_SENSOR_IR;
+extern const int oni_SENSOR_COLOR;
+extern const int oni_SENSOR_DEPTH;
 
 // ==============
 // openni::Status
@@ -63,6 +78,19 @@ extern const int oni_STATUS_NO_DEVICE;
 extern const int oni_STATUS_TIME_OUT;
 #endif
 
+// Common typedefs
+
+// ===================
+// openni::oni_Version
+// ===================
+typedef struct {
+    int build;
+    int maintenance;
+    int major;
+    int minor;
+} oni_Version;
+
+
 // =============
 // openni::Array
 // =============
@@ -73,24 +101,28 @@ extern const int oni_STATUS_TIME_OUT;
 // ======================
 bool oni_getAutoExposureEnabled(oni_CameraSettings * camera);
 bool oni_getAutoWhiteBalanceEnabled(oni_CameraSettings * camera);
-bool oni_isValid(oni_CameraSettings * camera);
+bool oni_CameraSettings_isValid(oni_CameraSettings * camera);
 oni_Status oni_setAutoExposureEnabled(oni_CameraSettings * camera, bool enabled);
 oni_Status oni_setAutoWhiteBalanceEnabled(oni_CameraSettings * camera, bool enabled);
 
 // ===========================
 // openni::CoordinateConverter
 // ===========================
-oni_Status oni_convertDepthToColor(oni_VideoStream * depth, oni_VideoStream * color, int depthX, int depthY, oni_DepthPixel depthZ, int *pColorX, int *pColorY);
-oni_Status oni_convertDepthToWorld1(oni_VideoStream * depth, int depthX, int depthY, oni_DepthPixel depthZ, float *pWorldX, float *pWorldY, float *pWorldZ);
-oni_Status oni_convertDepthToWorld2(oni_VideoStream * depth, float depthX, float depthY, float depthZ, float *pWorldX, float *pWorldY, float *pWorldZ);
-oni_Status oni_convertWorldToDepth1(oni_VideoStream * depth, float worldX, float worldY, float worldZ, int *pDepthX, int *pDepthY, oni_DepthPixel *pDepthZ);
-oni_Status oni_convertWorldToDepth2(oni_VideoStream * depth, float worldX, float worldY, float worldZ, float *pDepthX, float *pDepthY, float *pDepthZ);
-
-// ==============
-// openni::OpenNI
-// ==============
-oni_Status oni_initialize();
-const char * oni_getExtendedError();
+oni_Status oni_convertDepthToColor(
+    oni_VideoStream * depth, oni_VideoStream * color, int depthX, int depthY,
+    oni_DepthPixel depthZ, int *pColorX, int *pColorY);
+oni_Status oni_convertDepthToWorld1(
+    oni_VideoStream * depth, int depthX, int depthY, oni_DepthPixel depthZ,
+    float *pWorldX, float *pWorldY, float *pWorldZ);
+oni_Status oni_convertDepthToWorld2(
+    oni_VideoStream * depth, float depthX, float depthY, float depthZ,
+    float *pWorldX, float *pWorldY, float *pWorldZ);
+oni_Status oni_convertWorldToDepth1(
+    oni_VideoStream * depth, float worldX, float worldY, float worldZ,
+    int *pDepthX, int *pDepthY, oni_DepthPixel *pDepthZ);
+oni_Status oni_convertWorldToDepth2(
+    oni_VideoStream * depth, float worldX, float worldY, float worldZ,
+    float *pDepthX, float *pDepthY, float *pDepthZ);
 
 // ==============
 // openni::Device
@@ -102,7 +134,22 @@ void oni_close(oni_Device * device);
 oni_DeviceInfo * oni_getDeviceInfo(oni_Device * device);
 oni_ImageRegistrationMode oni_getImageRegistrationMode(oni_Device * device);
 oni_PlaybackControl * oni_getPlaybackControl(oni_Device * device);
-int oni_open(oni_Device * device, const char * uri);
+oni_Status oni_getProperty(oni_Device * device, int propertyId, void * data, int * dataSize);
+// TODO (maybe?): templatized oni_getProperty?
+const oni_SensorInfo * oni_getSensorInfo(oni_Device * device, oni_SensorType sensorType);
+bool oni_hasSensor(oni_Device * device, oni_SensorType sensorType);
+oni_Status oni_invoke(oni_Device * device, int cmdId, const void * data, int dataSize);
+// TODO (maybe?): templatized oni_invoke?
+bool oni_isCommandSupported(oni_Device * device, int cmdId);
+bool oni_isFile(oni_Device * device);
+bool oni_isImageRegistrationModeSupported(oni_Device * device, oni_ImageRegistrationMode mode);
+bool oni_isPropertySupported(oni_Device * device, int propertyId);
+bool oni_Device_isValid(oni_Device * device);
+oni_Status oni_open(oni_Device * device, const char * uri);
+oni_Status oni_setDepthColorSyncEnabled(oni_Device * device, bool isEnabled);
+oni_Status oni_setImageRegistrationMode(oni_Device * device, oni_ImageRegistrationMode mode);
+oni_Status oni_setProperty(oni_Device * device, int propId, const void * data, int dataSize);
+// TODO (maybe?): templatized oni_setProperty?
 
 // ==================
 // openni::DeviceInfo
@@ -113,6 +160,22 @@ const char * oni_getUri(oni_DeviceInfo * info);
 uint16_t oni_getUsbProductId(oni_DeviceInfo * info);
 uint16_t oni_getUsbVendorId(oni_DeviceInfo * info);
 const char * oni_getVendor(oni_DeviceInfo * info);
+
+// ==============
+// openni::OpenNI
+// ==============
+oni_Status oni_addListener(oni_Listener * pListener);
+// TODO static void enumerateDevices(Array< DeviceInfo > *deviceInfoList)
+oni_Status oni_initialize();
+const char * oni_getExtendedError();
+oni_Version oni_getVersion();
+
+// ==================
+// openni::SensorInfo
+// ==================
+oni_SensorType oni_getSensorType();
+// Array<VideoMode> getSupportedVideoModes(); ?
+
 
 #ifdef __cplusplus
 } // extern "C"
